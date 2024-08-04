25077d74883de0a6f5ff01f1efd8851539305ac6
chef-nic <nicholasajohnson@me.com>
2024-08-03 19:45:03 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Nicholas Johnson on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
